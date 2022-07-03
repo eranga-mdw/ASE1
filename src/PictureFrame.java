269d53db156881a84058c9d3a60a1e1a4d382a5e
eranga_mdw <erangamdw@gmail.com>
2022-07-04 00:13:11 +0530

    rename method

--- src/PictureFrame.java
+++ src/PictureFrame.java
@@ -55,23 +55,21 @@ public class PictureFrame {
       }
     }
 
+    
     void drawDigitGivenCentre(Graphics g, int x, int y, int diameter, int n) {
       int radius = diameter / 2;
       g.setColor(Color.BLACK);
       // g.drawOval(x - radius, y - radius, diameter, diameter);
-      FontMetrics fm = g.getFontMetrics();
-      String txt = Integer.toString(n);
-      g.drawString(txt, x - fm.stringWidth(txt) / 2, y + fm.getMaxAscent() / 2);
+      drawFun(g,n,x,y);
     }
 
-    void drawDigitGivenCentre(Graphics g, int x, int y, int diameter, int n,
+
+    void drawDigitGivenDominoCentre(Graphics g, int x, int y, int diameter, int n,
         Color c) {
       int radius = diameter / 2;
       g.setColor(c);
       // g.drawOval(x - radius, y - radius, diameter, diameter);
-      FontMetrics fm = g.getFontMetrics();
-      String txt = Integer.toString(n);
-      g.drawString(txt, x - fm.stringWidth(txt) / 2, y + fm.getMaxAscent() / 2);
+      drawFun(g,n,x,y);
     }
 
     void fillDigitGivenCentre(Graphics g, int x, int y, int diameter, int n) {
